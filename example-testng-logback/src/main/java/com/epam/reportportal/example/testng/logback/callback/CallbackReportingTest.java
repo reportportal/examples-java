b85867e6d15d47bc526d72509e518b9275fa24da
yumfriez <budaevqwerty@gmail.com>
2019-11-27 13:07:24 +0300

    TestNG callback reporting example provided

--- example-testng-logback/src/main/java/com/epam/reportportal/example/testng/logback/callback/CallbackReportingTest.java
+++ example-testng-logback/src/main/java/com/epam/reportportal/example/testng/logback/callback/CallbackReportingTest.java
@@ -1,9 +1,18 @@
 package com.epam.reportportal.example.testng.logback.callback;
 
-import org.testng.ITestContext;
+import com.epam.reportportal.service.tree.ItemTreeReporter;
+import com.epam.reportportal.service.tree.TestItemTree;
+import com.epam.reportportal.testng.util.ItemTreeUtils;
+import com.epam.ta.reportportal.ws.model.FinishTestItemRQ;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
+import java.util.Calendar;
+
+import static com.epam.reportportal.testng.TestNGService.ITEM_TREE;
+import static com.epam.reportportal.testng.TestNGService.REPORT_PORTAL;
 
 /**
  * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
@@ -11,27 +20,36 @@ import java.io.IOException;
 public class CallbackReportingTest {
 
 	@Test
-	public void first(ITestContext testContext) throws IOException {
+	public void first() {
+		Assert.assertEquals(1, 1);
+	}
+
+	@AfterMethod
+	public void after(ITestResult testResult) {
+		TestItemTree.TestItemLeaf testResultLeaf = ItemTreeUtils.retrieveLeaf(testResult, ITEM_TREE);
+		if (testResultLeaf != null) {
+			sendLog(testResultLeaf);
+			sendFinishRequest(testResultLeaf);
+		}
 
 	}
 
-//	@AfterMethod
-//	public void after(ITestResult testResult) {
-//		TestItemTree.TestItemLeaf testResultLeaf = ItemTreeUtils.retrieveLeaf(testResult, ITEM_TREE);
-//		if (testResultLeaf != null) {
-//			//			ItemTreeReporter.sendLog(REPORT_PORTAL.getClient(),
-//			//					"ERROR",
-//			//					"Callback log",
-//			//					Calendar.getInstance().getTime(),
-//			//					new File("/Users/yumfriez/IdeaProjects/example-java-TestNG/logback/src/main/resources/files/payload.json"),
-//			//					testResultLeaf
-//			//			);
-//
-//			FinishTestItemRQ finishTestItemRQ = new FinishTestItemRQ();
-//			finishTestItemRQ.setStatus("FAILED");
-//			finishTestItemRQ.setEndTime(Calendar.getInstance().getTime());
-//			finishItem(REPORT_PORTAL.getClient(), finishTestItemRQ, ITEM_TREE.getLaunchId(), testResultLeaf).cache().subscribe();
-//		}
-//
-//	}
+	private void sendLog(TestItemTree.TestItemLeaf testResultLeaf) {
+		ItemTreeReporter.sendLog(REPORT_PORTAL.getClient(),
+				"ERROR",
+				"Callback log",
+				Calendar.getInstance().getTime(),
+				ITEM_TREE.getLaunchId(),
+				testResultLeaf
+		);
+	}
+
+	private void sendFinishRequest(TestItemTree.TestItemLeaf testResultLeaf) {
+		FinishTestItemRQ finishTestItemRQ = new FinishTestItemRQ();
+		finishTestItemRQ.setStatus("FAILED");
+		finishTestItemRQ.setEndTime(Calendar.getInstance().getTime());
+		ItemTreeReporter.finishItem(REPORT_PORTAL.getClient(), finishTestItemRQ, ITEM_TREE.getLaunchId(), testResultLeaf)
+				.cache()
+				.blockingGet();
+	}
 }
