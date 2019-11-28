566944352f2f496103ffb831c7782e93b77add45
yumfriez <budaevqwerty@gmail.com>
2019-11-28 19:11:39 +0300

    Test item attribute examples provided

---
+++ example-cucumber3/src/test/java/com/epam/reportportal/example/cucumber3/attribute/RunCukesTest.java
@@ -0,0 +1,10 @@
+package com.epam.reportportal.example.cucumber3.attribute;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "pretty", "com.epam.reportportal.cucumber.StepReporter" })
+public class RunCukesTest {
+}
---
+++ example-junit5/src/test/java/com/epam/reportportal/example/junit5/attribute/ItemAttributeTest.java
@@ -0,0 +1,34 @@
+package com.epam.reportportal.example.junit5.attribute;
+
+import com.epam.reportportal.annotations.attribute.Attribute;
+import com.epam.reportportal.annotations.attribute.Attributes;
+import com.epam.reportportal.annotations.attribute.MultiKeyAttribute;
+import com.epam.reportportal.annotations.attribute.MultiValueAttribute;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
+ */
+//@ExtendWith(ReportPortalExtension.class)
+public class ItemAttributeTest {
+
+	@Test
+	@Attributes(attributes = { @Attribute(key = "key", value = "value") })
+	public void first() {
+		Assertions.assertEquals(1, 1);
+	}
+
+	@Test
+	@Attributes(attributes = { @Attribute(key = "key1", value = "value1"),
+			@Attribute(key = "key2", value = "value2") }, multiKeyAttributes = { @MultiKeyAttribute(keys = { "k1", "k2" }, value = "v") })
+	public void second() {
+		Assertions.assertEquals(1, 1);
+	}
+
+	@Test
+	@Attributes(multiValueAttributes = { @MultiValueAttribute(isNullKey = true, values = { "v1", "v2" }) })
+	public void third() {
+		Assertions.assertEquals(1, 1);
+	}
+}
