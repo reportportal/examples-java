d6d4916f2ec781897e4eec64551dab64e48aacb2
yumfriez <budaevqwerty@gmail.com>
2019-12-04 21:51:39 +0300

    Nested steps example update

--- example-testng-logback/src/main/java/com/epam/reportportal/example/testng/logback/step/NestedStepsTest.java
+++ example-testng-logback/src/main/java/com/epam/reportportal/example/testng/logback/step/NestedStepsTest.java
@@ -4,8 +4,12 @@ import com.epam.reportportal.annotations.Step;
 import com.epam.reportportal.testng.ReportPortalTestNGListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
@@ -16,19 +20,133 @@ public class NestedStepsTest {
 	public static final Logger LOGGER = LoggerFactory.getLogger(NestedStepsTest.class);
 
 	@Test
-	void testWithNestedSteps() {
-		LOGGER.info("Main log");
-		firstStep("FIRST");
-	}
+	void orderProductsTest() {
 
-	@Step(value = "Name is {value}")
-	public void firstStep(String value) {
-		innerStep();
-		LOGGER.info("First step log");
+		final Integer productCount = 5;
+		final Double price = 3.0;
+		final Double totalPrice = price * productCount;
+
+		navigateToMainPage();
+		login();
+		navigateToProductsPage();
+		addProductToCart(productCount);
+		pay(totalPrice);
+		logout();
 	}
 
 	@Step
-	public void innerStep() {
-		LOGGER.info("Inner step log");
+	public void navigateToMainPage() {
+		LOGGER.info("Main page displayed");
+	}
+
+	@Step
+	public void login() {
+		OrderingSimulator.logIn();
+		LOGGER.info("User logged in");
+	}
+
+	@Step
+	public void navigateToProductsPage() {
+		List<String> products = OrderingSimulator.getProducts();
+		LOGGER.info("Products page opened");
+	}
+
+	@Step("Add {count} products to the cart")
+	public void addProductToCart(Integer count) {
+		String product = clickOnProduct();
+		selectProductsCount(count);
+		clickCartButton(product, count);
+	}
+
+	@Step
+	private String clickOnProduct() {
+		LOGGER.info("Product click event");
+		return OrderingSimulator.chooseProduct();
+	}
+
+	@Step("{method} with {count} products")
+	private void selectProductsCount(Integer count) {
+		LOGGER.info(count + " products selected");
+	}
+
+	@Step("{productCount} products added")
+	private void clickCartButton(String product, Integer productCount) {
+		OrderingSimulator.addProduct(product, productCount);
+		LOGGER.info(productCount + " products added to the cart");
+		Assert.assertEquals(5, productCount.intValue());
+	}
+
+	@Step("Payment step with price = {totalPrice}")
+	public void pay(Double totalPrice) {
+		OrderingSimulator.doPayment(totalPrice);
+		LOGGER.info("Successful payment");
+	}
+
+	@Step
+	public void logout() {
+		OrderingSimulator.logOut();
+		LOGGER.info("User logged out");
+	}
+
+	@Test
+	void orderProductsTestOld() {
+
+		final Integer productCount = 5;
+		final Double price = 3.0;
+		final Double totalPrice = price * productCount;
+
+		LOGGER.info("Main page displayed");
+
+		OrderingSimulator.logIn();
+		LOGGER.info("User logged in");
+
+		List<String> products = OrderingSimulator.getProducts();
+		LOGGER.info("Products page opened");
+
+		String product = OrderingSimulator.chooseProduct();
+		LOGGER.info("Product click event");
+
+		LOGGER.info(productCount + " products selected");
+
+		OrderingSimulator.addProduct(product, productCount);
+		LOGGER.info(productCount + " products added to the cart");
+		Assert.assertEquals(5, productCount.intValue());
+
+		OrderingSimulator.doPayment(totalPrice);
+		LOGGER.info("Successful payment");
+
+		OrderingSimulator.logOut();
+		LOGGER.info("User logged out");
+	}
+
+	private static class OrderingSimulator {
+
+		public static void logIn() {
+			initChromeDriver();
+		}
+
+		private static void initChromeDriver() {
+
+		}
+
+		public static List<String> getProducts() {
+			return new ArrayList<>();
+		}
+
+		public static String chooseProduct() {
+			return "productName";
+		}
+
+		public static void addProduct(String name, Integer count) {
+
+		}
+
+		public static void doPayment(Double price) {
+
+		}
+
+		public static void logOut() {
+
+		}
 	}
 }
