daa93169b5476b64ae844a1c13f4c920e1e28409
Aliaksandr Heleisha <Aliaksandr_Heleisha@epam.com>
2020-01-14 10:55:14 +0300

    Tests for 4Cucumber

---
+++ example-cucumber4/src/test/java/com/epam/reportportal/example/cucumber4/logging/ReportsTestWithParameters.java
@@ -0,0 +1,22 @@
+package com.epam.reportportal.example.cucumber4.logging;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReportsTestWithParameters {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReportAttachmentsTest.class);
+
+    @Given("It is test with parameters")
+    public void infoLevel() {
+        LOGGER.info("It is test with parameters");
+    }
+
+    @Then("I emit number {int} on level info")
+    public void infoLevel(int parameters) {
+        LOGGER.info("Test with parameters: " + parameters);
+    }
+}
