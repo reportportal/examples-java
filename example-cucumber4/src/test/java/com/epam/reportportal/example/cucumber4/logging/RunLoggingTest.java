99f7e6d3eeeac8db7cd556a3415dead558dbeded
Aliaksandr Heleisha <Aliaksandr_Heleisha@epam.com>
2020-01-21 11:10:20 +0300

    Rename runner

--- example-cucumber4/src/test/java/com/epam/reportportal/example/cucumber4/logging/RunLoggingTest.java
+++ example-cucumber4/src/test/java/com/epam/reportportal/example/cucumber4/logging/RunLoggingTest.java
@@ -7,5 +7,5 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "pretty",
 		"com.epam.reportportal.cucumber.StepReporter" }, features = "src/test/resources/com/epam/reportportal/example/cucumber4/logging", tags = "not @ignore")
-public class RunCukesTest {
+public class RunLoggingTest {
 }
