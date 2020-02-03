bd31f738a5e33fee59885a62f43dc4f12f74e13f
yumfriez <budaevqwerty@gmail.com>
2020-02-03 18:16:23 +0300

    Nested steps example provided for JUnit5 agent

---
+++ example-junit5/src/test/java/com/epam/reportportal/example/junit5/nested/NestedSteps.java
@@ -0,0 +1,81 @@
+package com.epam.reportportal.example.junit5.nested;
+
+import com.epam.reportportal.annotations.Step;
+import com.epam.reportportal.junit5.ReportPortalExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
+ */
+@Execution(ExecutionMode.CONCURRENT)
+@ExtendWith(ReportPortalExtension.class)
+public class NestedSteps {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(NestedSteps.class);
+
+	private static final String METHODS_NAME_TEMPLATE = "My name is - `{method}` and I am a nested step of the `{parent}`";
+
+	private static ThreadLocal<Integer> counter = new InheritableThreadLocal<Integer>() {
+		@Override
+		protected Integer initialValue() {
+			return 0;
+		}
+	};
+
+	@BeforeEach
+	public void beforeEach() {
+		first();
+	}
+
+	@Test
+	void firstTest() {
+		LOGGER.error("yeah");
+		greenNestedStep("firstTest");
+	}
+
+	@Test
+	void secondTest() {
+		redNestedStep("secondTest");
+	}
+
+	@Step(value = METHODS_NAME_TEMPLATE)
+	public void greenNestedStep(String parent) {
+		LOGGER.error("hello");
+		if (counter.get() == 5) {
+			return;
+		}
+		counter.set(counter.get() + 1);
+		redNestedStep("greenNestedStep");
+
+	}
+
+	@Step(value = "My name is - `{method}` and I am a nested step of the `{parent}`")
+	public void redNestedStep(String parent) {
+		LOGGER.error("hello");
+		greenNestedStep("redNestedStep");
+	}
+
+	@Step
+	public void first() {
+		LOGGER.error("hello");
+		second();
+	}
+
+	@Step
+	public void second() {
+		LOGGER.error("hello");
+		third();
+	}
+
+	@Step
+	public void third() {
+		LOGGER.error("hello");
+	}
+
+}
