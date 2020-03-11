3d45c4d3ebd40d7f98d46d62f942c1e78930f9b0
ihar-kahadouski <igor.kagadowski@gmail.com>
2020-03-11 15:00:08 +0300

    update cucumber3 example with parameters

--- example-cucumber3/src/test/java/com/epam/reportportal/example/cucumber3/logging/ReportsTestWithParameters.java
+++ example-cucumber3/src/test/java/com/epam/reportportal/example/cucumber3/logging/ReportsTestWithParameters.java
@@ -2,16 +2,22 @@ package com.epam.reportportal.example.cucumber3.logging;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ReportsTestWithParameters {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ReportAttachmentsTest.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReportsTestWithParameters.class);
 
 	@Given("It is test with parameters")
 	public void infoLevel() {
 		LOGGER.info("It is test with parameters");
+	}
+
+	@When("I have parameter (.*)")
+	public void iHaveParameterStr(String str) {
+		LOGGER.info("String parameter {}", str);
 	}
 
 	@Then("I emit number {int} on level info")
