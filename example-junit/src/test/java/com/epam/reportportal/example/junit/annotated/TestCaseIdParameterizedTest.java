bea183f3bd07066d711bf2146b0c6cd87d67b30a
Ivan Budayeu <ivan_budayeu@epam.com>
2020-03-20 11:16:07 +0300

    JUnit4 parametrized test update

--- example-junit/src/test/java/com/epam/reportportal/example/junit/annotated/TestCaseIdParameterizedTest.java
+++ example-junit/src/test/java/com/epam/reportportal/example/junit/annotated/TestCaseIdParameterizedTest.java
@@ -5,6 +5,7 @@ import com.epam.reportportal.annotations.TestCaseIdKey;
 import com.google.common.base.Optional;
 import com.nordstrom.automation.junit.ArtifactParams;
 import com.nordstrom.automation.junit.AtomIdentity;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.Description;
@@ -46,11 +47,16 @@ public class TestCaseIdParameterizedTest implements ArtifactParams {
 	}
 
 	@Test
-	@TestCaseId(parametrized = true)
 	public void parameterized() {
 		Optional<Map<String, Object>> params = identity.getParameters();
 		assertTrue(params.isPresent());
 		assertTrue(params.get().containsKey("input"));
 		assertEquals(input, params.get().get("input"));
+	}
+
+	@TestCaseId("JU4.1.0")
+	@Test
+	public void simpleTest() {
+		Assert.assertEquals(1, 1);
 	}
 }
