465df0133468743cb8eadf692bb695532faacdd6
ihar-kahadouski <igor.kagadowski@gmail.com>
2020-03-27 14:07:40 +0300

    add launch logs example

---
+++ example-cucumber4/src/test/java/com/epam/reportportal/example/cucumber4/util/AttachmentHelper.java
@@ -0,0 +1,22 @@
+package com.epam.reportportal.example.cucumber4.util;
+
+import rp.com.google.common.io.Files;
+import rp.com.google.common.io.Resources;
+
+import java.io.File;
+import java.io.IOException;
+
+public class AttachmentHelper {
+
+	public static final String FILE_NAME = "file";
+
+	public static File getFileFromResources(String name, String extension) {
+		File file = null;
+		try {
+			file = File.createTempFile("rp-test", extension);
+			Resources.asByteSource(Resources.getResource(String.format("files/%s%s", name, extension))).copyTo(Files.asByteSink(file));
+		} catch (IOException ignored) {
+		}
+		return file;
+	}
+}
