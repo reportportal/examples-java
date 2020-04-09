74d8849c3bb6ce5a28484360fd21333a30579b27
Alena Liamtsava <a.liamtsava@gmail.com>
2020-04-09 14:37:49 +0300

    Add code to reproduce bug

--- example-junit/src/test/java/com/epam/reportportal/example/junit/extension/RetryTest.java
+++ example-junit/src/test/java/com/epam/reportportal/example/junit/extension/RetryTest.java
@@ -1,12 +1,14 @@
 package com.epam.reportportal.example.junit.extension;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Ignore
 public class RetryTest {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(RetryTest.class);
@@ -14,6 +16,7 @@ public class RetryTest {
 	private static final int RETRY_NUMBER = 20;
 	private static final AtomicInteger COUNTER = new AtomicInteger(0);
 
+//	@Ignore
 	@Test
 	public void failOne() {
 		String errorMsg = "Ooops";
