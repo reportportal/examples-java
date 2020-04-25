e9e57d5a16af54f5844bc5ab0ee071827f52d11e
vadzim hushchanskou <vadzim_hushchanskou@epam.com>
2020-04-25 21:53:37 +0300

    Unignore retry test and update resources

--- example-junit/src/test/java/com/epam/reportportal/example/junit/SimpleTest.java
+++ example-junit/src/test/java/com/epam/reportportal/example/junit/SimpleTest.java
@@ -1,17 +1,12 @@
 package com.epam.reportportal.example.junit;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
 
-@Ignore
 public class SimpleTest {
 
-//    @Ignore
-    @Test
-    public void simpleTest() {
-        Assert.assertEquals(3, 3);
-    }
+	@Test
+	public void simpleTest() {
+		Assert.assertEquals(3, 3);
+	}
 }
