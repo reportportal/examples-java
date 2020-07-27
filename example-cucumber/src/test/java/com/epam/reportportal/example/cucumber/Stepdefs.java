67542a187d8332b91fa847e0e99ee677126826a9
Ivan Budayeu <ivan_budayeu@epam.com>
2020-07-27 12:35:16 +0300

    Formatting fixed

--- example-cucumber/src/test/java/com/epam/reportportal/example/cucumber/Stepdefs.java
+++ example-cucumber/src/test/java/com/epam/reportportal/example/cucumber/Stepdefs.java
@@ -9,24 +9,24 @@ import cucumber.api.java.en.When;
 import static org.junit.Assert.assertTrue;
 
 public class Stepdefs {
-	
+
 	private Belly belly = new Belly();
 
 	@TestCaseId(parametrized = true)
-    @Given("^I have (\\d+) cukes in my belly$")
-    public void I_have_cukes_in_my_belly(@TestCaseIdKey int cukes) {
-        belly.eat(cukes);
-    }
+	@Given("^I have (\\d+) cukes in my belly$")
+	public void I_have_cukes_in_my_belly(@TestCaseIdKey int cukes) {
+		belly.eat(cukes);
+	}
 
-    @TestCaseId(parametrized = true)
-    @When("^I wait (\\d+) hour$")
-    public void I_wait(int hours) {
-    	belly.wait(hours);
-    }
+	@TestCaseId(parametrized = true)
+	@When("^I wait (\\d+) hour$")
+	public void I_wait(int hours) {
+		belly.wait(hours);
+	}
 
-    @TestCaseId(value = "C1.1.0")
-    @Then("^my belly should growl$")
-    public void my_belly_should_growl() {
-    	assertTrue(belly.growl());
-    }
+	@TestCaseId(value = "C1.1.0")
+	@Then("^my belly should growl$")
+	public void my_belly_should_growl() {
+		assertTrue(belly.growl());
+	}
 }
