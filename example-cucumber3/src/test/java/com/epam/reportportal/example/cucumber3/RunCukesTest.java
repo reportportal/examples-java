1617993c4fa519fc3efc89bd54f05f286188c3a6
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-10-06 16:36:34 +0300

    Cucumber 3 examples fix

--- example-cucumber3/src/test/java/com/epam/reportportal/example/cucumber3/RunCukesTest.java
+++ example-cucumber3/src/test/java/com/epam/reportportal/example/cucumber3/RunCukesTest.java
@@ -5,6 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "pretty", "com.epam.reportportal.cucumber.StepReporter" }, tags = "~@ignore")
+@CucumberOptions(plugin = { "pretty", "com.epam.reportportal.cucumber.StepReporter" },
+		features = {"src/test/resources/features/attribute"},
+		glue = {"com.epam.reportportal.example.cucumber3.attribute"})
 public class RunCukesTest {
 }
