0ddcd7012357915504fcb7080ae11551bc7a4c22
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-03-24 17:55:13 +0300

    Screenshot examples for cucumber 6

---
+++ example-cucumber6/src/test/java/com/epam/reportportal/example/cucumber6/logging/ScreenshotOnFailureHooks.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2021 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.reportportal.example.cucumber6.logging;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.en.Given;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
+
+/**
+ * The simplest way to report a screenshot on a test failure. Reports screenshot as a separate log entry into 'After hooks' step.
+ */
+public class ScreenshotOnFailureHooks {
+	private WebDriver driver;
+
+	@Before
+	public void beforeScenario() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+
+	@Given("I report scenario with screenshot")
+	public void test() {
+		driver.navigate().to("https://www.example.com");
+		Assert.assertEquals("Google", driver.getTitle());
+	}
+
+	@After
+	public void afterScenario(Scenario scenario) {
+		takeScreenshot(scenario);
+		driver.quit();
+	}
+
+	private void takeScreenshot(Scenario scenario) {
+		if (scenario.isFailed()) {
+			if (driver instanceof TakesScreenshot) {
+				scenario.attach(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES), "image/png", "Screenshot");
+			}
+		}
+	}
+}
---
+++ example-cucumber6/src/test/resources/features/logging/screenshot.feature
@@ -0,0 +1,4 @@
+Feature: Screenshot reporting
+
+  Scenario: Screenshot
+    Given I report scenario with screenshot
