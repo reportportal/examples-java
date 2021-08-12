e2b8e85d467894457c64d2676b67124c827e9bfe
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-08-12 17:34:19 +0300

    Another spock example

---
+++ example-spock/src/test/groovy/HelloSpockSpecUnroll.groovy
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class HelloSpockSpecUnroll extends Specification {
+    public static final String TEST_NAME = "length of Spock's and his friends' names"
+
+    @Unroll
+    def "length of Spock's and his friends' names" () {
+        expect:
+        name.size() == length
+
+        where:
+        name     | length
+        "Spock"  | 5
+        "Kirk"   | 4
+        "Scotty" | 6
+    }
+}
