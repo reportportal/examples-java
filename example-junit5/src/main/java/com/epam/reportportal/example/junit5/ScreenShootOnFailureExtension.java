350e65b99bda14e25322c7f052ad263d042247df
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-04-29 17:52:01 +0300

    JUnit 5 selenide screenshot on failure example test

---
+++ example-junit5/src/main/java/com/epam/reportportal/example/junit5/ScreenShootOnFailureExtension.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.reportportal.example.junit5;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.InvocationInterceptor;
+import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
+import org.openqa.selenium.OutputType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+import java.util.Base64;
+
+/**
+ * JUnit 5the extension which makes a Selenide screenshot in case of test failure.
+ */
+public class ScreenShootOnFailureExtension implements InvocationInterceptor {
+	private static final Logger LOGGER = LoggerFactory.getLogger("binary_data_logger");
+
+	/**
+	 * Make a screenshot with Selenide and attach it to the current test item on Report Portal in case of test failure.
+	 *
+	 * @param invocation        JUnit 5th invocation object
+	 * @param invocationContext JUnit 5th invocation context
+	 * @param extensionContext  JUnit 5th extension context
+	 */
+	@Override
+	public void interceptTestMethod(Invocation<Void> invocation, ReflectiveInvocationContext<Method> invocationContext,
+			ExtensionContext extensionContext) throws Throwable {
+		try {
+			invocation.proceed();
+		} catch (Throwable cause) {
+			byte[] screenshot = Selenide.screenshot(OutputType.BYTES);
+			LOGGER.info("RP_MESSAGE#BASE64#{}#{}", Base64.getEncoder().encodeToString(screenshot), "Failure screenshot");
+			throw cause;
+		}
+	}
+}
---
+++ example-junit5/src/test/java/com/epam/reportportal/example/junit5/logging/SelenideScreenshotLoggingTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.reportportal.example.junit5.logging;
+
+import com.codeborne.selenide.Browsers;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.epam.reportportal.example.junit5.ScreenShootOnFailureExtension;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(ScreenShootOnFailureExtension.class)
+public class SelenideScreenshotLoggingTest {
+
+	@BeforeAll
+	public static void initDriver() {
+		WebDriverManager.chromedriver().setup();
+		Configuration.browser = Browsers.CHROME;
+	}
+
+	@Test
+	public void testTitle() {
+		Selenide.open("https://www.example.com");
+		assertEquals(Selenide.title(), "Google");
+	}
+
+	@AfterEach
+	public void tearDown() {
+		Selenide.closeWebDriver();
+	}
+}
