021d22d3f898e1adb1a0d2f9327e3e8d733bf48d
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-12-05 18:56:48 +0300

    Add more Spock tests

---
+++ example-spock/src/test/groovy/attributes/FixtureAttributes.groovy
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package attributes
+
+import com.epam.reportportal.annotations.attribute.Attribute
+import com.epam.reportportal.annotations.attribute.Attributes
+import spock.lang.Specification
+
+class FixtureAttributes extends Specification {
+
+    @Attributes(attributes = @Attribute(key = "key", value = "value"))
+    def "my empty fixture"() {
+        expect:
+        //noinspection GroovyPointlessBoolean
+        true == true
+    }
+}
