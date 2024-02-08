d84bbf60819b01064e6a605d79de75f90fc0d2d0
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2024-02-08 18:43:49 +0300

    Fix build

--- example-cucumber/src/test/java/com/epam/reportportal/example/cucumber/util/MagicRandomizer.java
+++ example-cucumber/src/test/java/com/epam/reportportal/example/cucumber/util/MagicRandomizer.java
@@ -2,8 +2,6 @@ package com.epam.reportportal.example.cucumber.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.google.common.base.Preconditions;
-import com.google.common.collect.Range;
 
 import java.util.Random;
 
@@ -16,9 +14,8 @@ public class MagicRandomizer {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(MagicRandomizer.class);
 
-	private static final Range<Integer> PROBABILITY_RANGE = Range.openClosed(0, 100);
-
 	private static final Random RANDOM = new Random();
+	private static final int UPPER_LIMIT = 100;
 
 	private MagicRandomizer() {
 		//statics only
@@ -35,10 +32,7 @@ public class MagicRandomizer {
 	 * @return TRUE if you are really lucky!
 	 */
 	public static boolean checkYourLucky(int probability) {
-		Preconditions.checkArgument(PROBABILITY_RANGE.contains(probability), "%s is not in range [%s]", probability, PROBABILITY_RANGE);
-
-		boolean lucky = Range.closedOpen(PROBABILITY_RANGE.lowerEndpoint(), probability)
-				.contains(luckyInt(PROBABILITY_RANGE.upperEndpoint()));
+		boolean lucky = luckyInt(UPPER_LIMIT + 1) <= probability;
 		LOGGER.debug("Generating [TRUE/FALSE] with probability {}%. Result {}", probability, lucky);
 		return lucky;
 	}
