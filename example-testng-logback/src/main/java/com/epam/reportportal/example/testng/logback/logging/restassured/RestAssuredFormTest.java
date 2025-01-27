82db194c96cdca413a364c1e4ce232eb8da27434
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-01-27 17:07:34 +0300

    Example update

--- example-testng-logback/src/main/java/com/epam/reportportal/example/testng/logback/logging/restassured/RestAssuredFormTest.java
+++ example-testng-logback/src/main/java/com/epam/reportportal/example/testng/logback/logging/restassured/RestAssuredFormTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022 EPAM Systems
+ * Copyright 2025 EPAM Systems
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,7 +23,6 @@ import com.epam.reportportal.listeners.LogLevel;
 import com.epam.reportportal.restassured.ReportPortalRestAssuredLoggingFilter;
 import com.epam.reportportal.service.Launch;
 import io.restassured.RestAssured;
-import org.jetbrains.annotations.NotNull;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -66,7 +65,7 @@ public class RestAssuredFormTest {
 	 * @param prettifiers original prettifier map
 	 * @return updated prettifier map
 	 */
-	@NotNull
+	@Nonnull
 	private static Map<String, Function<String, String>> getUpdatedPrettifiersMap(@Nonnull Map<String, Function<String, String>> prettifiers) {
 		Map<String, Function<String, String>> myPrettifiers = new HashMap<>(prettifiers);
 		Function<String, String> jsonPrettifier = myPrettifiers.get("application/json");
