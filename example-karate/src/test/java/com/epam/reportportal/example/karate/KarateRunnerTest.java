fad42233e8a6a7a2707b3dcd016ef1f12e7e4efa
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-10-16 17:06:17 +0300

    Update karate example

--- example-karate/src/test/java/com/epam/reportportal/example/karate/KarateRunnerTest.java
+++ example-karate/src/test/java/com/epam/reportportal/example/karate/KarateRunnerTest.java
@@ -1,7 +1,6 @@
 package com.epam.reportportal.example.karate;
 
 import com.epam.reportportal.karate.KarateReportPortalRunner;
-import com.epam.reportportal.karate.ReportPortalHook;
 import com.intuit.karate.Results;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,12 +9,11 @@ public class KarateRunnerTest {
 
 	@Test
 	public void testAll() {
-		ReportPortalHook hook = new ReportPortalHook();
-		Results results = KarateReportPortalRunner.path("classpath:features")
-				.hook(hook)
-				.outputCucumberJson(true)
-				.tags("~@ignore")
-				.parallel(2);
+		Results results = KarateReportPortalRunner // Our runner with a Karate Hook
+				.path("classpath:features") // Path with feature files
+				.outputCucumberJson(true) // Generate cucumber report
+				.tags("~@ignore") // Ignore tests marked with the tag
+				.parallel(2); // Run in 2 Threads
 		Assertions.assertEquals(0, results.getFailCount(), "Non-zero fail count.\n Errors:\n" + results.getErrorMessages());
 	}
 }
