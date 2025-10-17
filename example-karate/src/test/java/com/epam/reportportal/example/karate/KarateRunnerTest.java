7cdbae86c1d02d769b2fecebe6f0a0d0746b0b30
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-10-17 16:24:38 +0300

    Update Karate example

--- example-karate/src/test/java/com/epam/reportportal/example/karate/KarateRunnerTest.java
+++ example-karate/src/test/java/com/epam/reportportal/example/karate/KarateRunnerTest.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2025 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.epam.reportportal.example.karate;
 
-import com.epam.reportportal.karate.KarateReportPortalRunner;
+import com.epam.reportportal.karate.ReportPortalHook;
 import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +26,15 @@ public class KarateRunnerTest {
 
 	@Test
 	public void testAll() {
-		Results results = KarateReportPortalRunner // Our runner with a Karate Hook
+		ReportPortalHook karateHook = new ReportPortalHook(); // Initialize ReportPortal runtime Karate hook
+		Results results = Runner // Regular Karate runner
 				.path("classpath:features") // Path with feature files
+				.hook(karateHook) // Add Karate hook
 				.outputCucumberJson(true) // Generate cucumber report
 				.tags("~@ignore") // Ignore tests marked with the tag
 				.parallel(2); // Run in 2 Threads
+		// Here you can additionally run tests, retries, etc.
+		karateHook.finishLaunch(); // Finish execution on ReportPortal
 		Assertions.assertEquals(0, results.getFailCount(), "Non-zero fail count.\n Errors:\n" + results.getErrorMessages());
 	}
 }
